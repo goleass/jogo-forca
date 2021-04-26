c9a296567ea50fd2f0b015de409aa7e0c612adf3
leonardo.assuncao <leonardo.assuncao>
2021-04-26 20:39:35 -0300

    1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
 const app = express();
 app.use(cors())
