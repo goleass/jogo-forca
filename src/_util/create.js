07d5c1b1769dd9bd403b42942dece56b2d8aea80
leonardo.assuncao <leonardo.assuncao>
2021-04-27 07:37:29 -0300

    modificando sql

--- src/_util/create.js
+++ src/_util/create.js
@@ -1,7 +1,7 @@
 module.exports = `
 CREATE TABLE if not exists public.usuarios
 (
-    pk_cod_usuario integer NOT NULL DEFAULT nextval('usuarios_pk_cod_usuario_seq'::regclass),
+    pk_cod_usuario serial,
     nome_usuario character varying(100) COLLATE pg_catalog."default",
     usuario character varying(100) COLLATE pg_catalog."default" NOT NULL,
     senha character varying(100) COLLATE pg_catalog."default" NOT NULL,
@@ -13,9 +13,9 @@ TABLESPACE pg_default;
 ALTER TABLE public.usuarios
     OWNER to postgres;
 
-CREATE TABLE if not exists public.categorias
+CREATE TABLE if not exists public.usuarios_pk_cod_usuario_seqcategorias
 (
-    pk_cod_categoria integer NOT NULL DEFAULT nextval('categorias_pk_cod_categoria_seq'::regclass),
+    pk_cod_categoria serial,
     nome_categoria character varying(100) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT categorias_pkey PRIMARY KEY (pk_cod_categoria)
 )
@@ -27,7 +27,7 @@ ALTER TABLE public.categorias
 
 CREATE TABLE if not exists public.palavras
 (
-    pk_cod_palavra integer NOT NULL DEFAULT nextval('palavras_pk_cod_palavra_seq'::regclass),
+    pk_cod_palavra serial,
     nome_palavra character varying(100) COLLATE pg_catalog."default" NOT NULL,
     fk_cod_categoria integer,
     dificuldade smallint,
